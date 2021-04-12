2913f9bbaae36b263e5c1334e89fa3fae510cd5c
krisztianteller <75849856+krisztianteller@users.noreply.github.com>
2021-04-12 09:58:35 +0200

    user input

--- logic.js
+++ logic.js
@@ -1,8 +1,10 @@
 
-//creating a random outcome
+let playerScore = 0;
+let computerScore = 0;
 
+//creating a random outcome
 function computerPlay(){
-   const number = Math.floor(Math.random() * 1000);
+   const number = Math.floor(Math.random() * 3);
    if( number % 3 === 0){
        return "rock";
    } 
@@ -14,20 +16,36 @@ function computerPlay(){
    }
 };
 
+
+//checking winner
 function playRound(playerSelection, computerSelection){
     if(playerSelection === "rock" && computerSelection === "paper"){
         return "You lose! Paper beats Rock.";
-    }
+}
     if(playerSelection === "paper" && computerSelection === "scissors"){
         return "You lose! Scissors beats paper.";
-    }
+}
     if(playerSelection === "scissors" && computerSelection === "rock"){
         return "You lose! Rock beats Scissors.";
-    } else {
-        return "You won"
-    }
-
+} else {
+        return "You win!";
 }
+
+};
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Some input', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
 
 const playerSelection = "paper";
 const computerSelection = computerPlay();
